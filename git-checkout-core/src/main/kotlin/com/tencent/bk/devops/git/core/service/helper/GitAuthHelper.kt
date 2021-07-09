dae72697a237772925a25d9521b8d3a7f87cc09e
mingshewhe <mingshewhe@tencent.com>
2021-07-09 17:50:33 +0800

    去掉XDG_CONFIG_HOME环境变量

--- git-checkout-core/src/main/kotlin/com/tencent/bk/devops/git/core/service/helper/GitAuthHelper.kt
+++ git-checkout-core/src/main/kotlin/com/tencent/bk/devops/git/core/service/helper/GitAuthHelper.kt
@@ -37,7 +37,6 @@ import com.tencent.bk.devops.git.core.pojo.CredentialArguments
 import com.tencent.bk.devops.git.core.pojo.GitSourceSettings
 import com.tencent.bk.devops.git.core.service.GitCommandManager
 import com.tencent.bk.devops.git.core.util.CommandUtil
-import com.tencent.bk.devops.git.core.util.EnvHelper
 import com.tencent.bk.devops.git.core.util.GitUtil
 import com.tencent.bk.devops.git.core.util.SSHAgentUtils
 import java.io.File
