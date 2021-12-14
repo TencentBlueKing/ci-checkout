c761e795cfc7d9f8560a5655d850a956aef9d473
mingshewhe <mingshewhe@tencent.com>
2021-12-14 22:05:43 +0800

    bug: 修复增加命名空间后，凭证无法传递问题
    原因是因为增加命名空间后，环境变量名被修改了，导致git-checkout-credential.sh查找不到变量

--- git-checkout-core/src/main/resources/script/git-checkout-credential.sh
+++ git-checkout-core/src/main/resources/script/git-checkout-credential.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-credential_java_path=`eval echo '$'credential_java_path_$BK_CI_BUILD_JOB_ID`
+credential_key='credential_java_path_'$BK_CI_BUILD_JOB_ID
+credential_java_path=$(env | grep "$credential_key" | awk -F '=' '{print $2}')
 if [[ -z $credential_java_path ]]; then
         exit 1
 fi
