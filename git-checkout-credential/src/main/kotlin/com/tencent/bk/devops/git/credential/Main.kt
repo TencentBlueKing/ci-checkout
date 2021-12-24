7e22930c3f24c06d4557368e4cd5a2fb8f21162b
mingshewhe <mingshewhe@tencent.com>
2021-12-24 14:22:32 +0800

    bug: 拉取时出现fatal: $HOME not set错误 #2
    调试

--- git-checkout-credential/src/main/kotlin/com/tencent/bk/devops/git/credential/Main.kt
+++ git-checkout-credential/src/main/kotlin/com/tencent/bk/devops/git/credential/Main.kt
@@ -35,6 +35,7 @@ fun main(args: Array<String>) {
         val program = Program(System.`in`, System.out)
         program.innerMain(args)
     } catch (ignore: Throwable) {
+        ignore.printStackTrace()
         System.err.println("Fatal: ${ignore.javaClass.name} encountered.  Details:")
         System.err.println(ignore.message)
     }
