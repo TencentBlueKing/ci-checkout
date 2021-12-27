be2807e65532a9b649e9f288a7fb0b063502abf5
mingshewhe <mingshewhe@tencent.com>
2021-12-27 15:27:03 +0800

    bug: 凭证无法读取到JAVA_HOME路径 #6

--- git-checkout-core/src/main/resources/script/git-checkout-credential.sh
+++ git-checkout-core/src/main/resources/script/git-checkout-credential.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 credential_key='credential_java_path_'$BK_CI_BUILD_JOB_ID
-credential_java_path=$(env | grep "$credential_key" | awk -F '=' '{print $2}')
+credential_java_path=$(env | grep "$credential_key" | awk -F '=' 'NR==1 {print $2}')
 if [[ -z $credential_java_path ]]; then
         exit 1
 fi
