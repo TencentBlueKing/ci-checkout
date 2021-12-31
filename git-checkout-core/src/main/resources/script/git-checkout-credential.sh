7feddab3dfef4ca178e5713307bee59180d2d8dd
mingshewhe <mingshewhe@tencent.com>
2021-12-31 12:10:11 +0800

    bug: 当多个job时,git凭证管理读取的java环境变量路径错误 #14

--- git-checkout-core/src/main/resources/script/git-checkout-credential.sh
+++ git-checkout-core/src/main/resources/script/git-checkout-credential.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 credential_key='credential_java_path_'$BK_CI_BUILD_JOB_ID
-credential_java_path=$(env | grep "$credential_key" | awk -F '=' 'NR==1 {print $2}')
+credential_java_path=$(env | grep "^[a-zA-Z0-9_]*$credential_key=" | awk '{sub(/^[^=]+=/,"");print; exit;}')
 if [[ -z $credential_java_path ]]; then
         exit 1
 fi
