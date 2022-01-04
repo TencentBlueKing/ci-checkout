4e0d5da6c0fdde9624891595a45c55b240328e23
v_jinleiliu <v_jinleiliu@tencent.com>
2022-01-04 16:08:28 +0800

    feat: 度量提取拉取速率 #12
    代码优化

--- git-checkout-core/src/main/kotlin/com/tencent/bk/devops/git/core/pojo/GitPackingPhase.kt
+++ git-checkout-core/src/main/kotlin/com/tencent/bk/devops/git/core/pojo/GitPackingPhase.kt
@@ -1,10 +1,10 @@
 package com.tencent.bk.devops.git.core.pojo
 
 data class GitPackingPhase(
-    var counting: String,
-    var findingSources: String,
-    var gettingSize: String,
-    var writing: String,
-    var transferRate: String,
-    var totalSize: String
+    val counting: String,
+    val findingSources: String,
+    val gettingSize: String,
+    val writing: String,
+    val transferRate: String,
+    val totalSize: String
 )
