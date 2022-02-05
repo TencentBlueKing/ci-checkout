8f2f4a047aed3ac903637f55c0ecbdc68cfbb40e
mingshewhe <mingshewhe@tencent.com>
2022-02-05 12:47:39 +0800

    bug: 拉取时出现fatal: $HOME not set错误 #2

--- git-checkout-core/src/main/resources/script/git-checkout-credential.sh
+++ git-checkout-core/src/main/resources/script/git-checkout-credential.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+bash_path=$(dirname $0)
 credential_key='credential_java_path_'$BK_CI_BUILD_JOB_ID
 credential_java_path=$(env | grep "^[a-zA-Z0-9_]*$credential_key=" | awk '{sub(/^[^=]+=/,"");print; exit;}')
 if [[ -z $credential_java_path ]]; then
         exit 1
 fi
-$credential_java_path -jar ./git-checkout-credential.jar $@
+$credential_java_path -jar $bash_path/git-checkout-credential.jar $@
