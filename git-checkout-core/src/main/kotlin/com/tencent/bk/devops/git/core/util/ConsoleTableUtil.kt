e1bb35c376d7bf55968dcf7fdb5fa644f8583176
mingshewhe <mingshewhe@tencent.com>
2022-07-01 10:58:03 +0800

    feat: 优化错误提示 #53

--- git-checkout-core/src/main/kotlin/com/tencent/bk/devops/git/core/util/ConsoleTableUtil.kt
+++ git-checkout-core/src/main/kotlin/com/tencent/bk/devops/git/core/util/ConsoleTableUtil.kt
@@ -5,21 +5,21 @@ import org.slf4j.LoggerFactory
 object ConsoleTableUtil {
 
     private val logger = LoggerFactory.getLogger(ConsoleTableUtil::class.java)
-    private const val format = "%s%-4s\t%s%-100s"
 
     fun printAsTable(errMsg: String, cause: String, solution: String) {
         logger.warn("************************************问题排查指引**********************************************")
         printSplit("错误信息", errMsg)
         printSplit("问题原因", cause)
         printSplit("解决办法", solution)
+        logger.warn("********************************************************************************************")
     }
 
     private fun printSplit(title: String, str: String) {
         str.split("\n").forEachIndexed { index, message ->
             if (index == 0) {
-                logger.warn(String.format(format, "|", title, "|", message.trim()))
+                logger.warn("$title:${message.trim()}")
             } else {
-                logger.warn(String.format(format, "|", "    ", "|", message.trim()))
+                logger.warn("       ${message.trim()}")
             }
         }
     }
