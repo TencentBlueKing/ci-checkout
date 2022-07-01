66453d519143dcd8fda531c980f6a4189b26c6ac
mingshewhe <mingshewhe@tencent.com>
2022-07-01 12:26:58 +0800

    feat: 优化错误提示 #53

--- git-checkout-core/src/main/kotlin/com/tencent/bk/devops/git/core/util/ConsoleTableUtil.kt
+++ git-checkout-core/src/main/kotlin/com/tencent/bk/devops/git/core/util/ConsoleTableUtil.kt
@@ -8,18 +8,18 @@ object ConsoleTableUtil {
 
     fun printAsTable(errMsg: String, cause: String, solution: String) {
         logger.warn("************************************问题排查指引**********************************************")
-        printSplit("错误信息", errMsg)
-        printSplit("问题原因", cause)
-        printSplit("解决办法", solution)
+        printSplit("【错误信息】", errMsg)
+        printSplit("【问题原因】", cause)
+        printSplit("【解决办法】", solution)
         logger.warn("********************************************************************************************")
     }
 
     private fun printSplit(title: String, str: String) {
         str.split("\n").forEachIndexed { index, message ->
             if (index == 0) {
-                logger.warn("$title:${message.trim()}")
+                logger.warn("$title：\t${message.trim()}")
             } else {
-                logger.warn("       ${message.trim()}")
+                logger.warn("           \t${message.trim()}")
             }
         }
     }
