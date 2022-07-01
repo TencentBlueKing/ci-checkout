32e899b8151d02adf0ee491d058a2f23887761cb
mingshewhe <mingshewhe@tencent.com>
2022-07-01 17:18:32 +0800

    feat: 优化错误提示 #53

--- git-checkout-core/src/main/resources/script/git-checkout.sh
+++ git-checkout-core/src/main/resources/script/git-checkout.sh
@@ -8,6 +8,7 @@ fi
 credential_jar_key='credential_jar_path_'$BK_CI_BUILD_JOB_ID
 credential_jar_path=$(env | grep "^[a-zA-Z0-9_]*$credential_jar_key=" | awk '{sub(/^[^=]+=/,"");print; exit;}')
 if [[ -z $credential_jar_path ]]; then
-        exit 1
+        $credential_java_path -jar $bash_path/$credential_jar_path $@
+elif [ -f $bash_path/git-checkout-credential.jar ]; then
+        $credential_java_path -jar $bash_path/git-checkout-credential.jar $@
 fi
-$credential_java_path -jar $bash_path/$credential_jar_path $@
