6ba7ce25301726cb4d3ffad05f4031547ed4eab6
mingshewhe <mingshewhe@tencent.com>
2024-07-19 13:07:09 +0800

    feat: 增加工蜂cache支持 #49
    度量细粒化,可以区分全量+工蜂cache,增量+工蜂cache

--- git-checkout-core/src/main/kotlin/com/tencent/bk/devops/git/core/GitCheckoutRunner.kt
+++ git-checkout-core/src/main/kotlin/com/tencent/bk/devops/git/core/GitCheckoutRunner.kt
@@ -57,7 +57,6 @@ import com.tencent.bk.devops.git.core.constant.ContextConstants.CONTEXT_USER_ID
 import com.tencent.bk.devops.git.core.constant.ContextConstants.CONTEXT_VM_EXIST_REPO
 import com.tencent.bk.devops.git.core.constant.GitConstants
 import com.tencent.bk.devops.git.core.constant.GitConstants.BK_CI_ATOM_CODE
-import com.tencent.bk.devops.git.core.enums.FetchStrategy
 import com.tencent.bk.devops.git.core.enums.GitProtocolEnum
 import com.tencent.bk.devops.git.core.exception.TaskExecuteException
 import com.tencent.bk.devops.git.core.pojo.GitMetricsInfo
