68e52686e53390f0536a38d41d1440a4af2d514a
hejieehe <904696180@qq.com>
2024-10-29 16:34:50 +0800

    feat: 工蜂cache支持按照策略开启 #166

--- git-checkout-core/src/main/kotlin/com/tencent/bk/devops/git/core/service/input/CheckoutAtomParamInputAdapter.kt
+++ git-checkout-core/src/main/kotlin/com/tencent/bk/devops/git/core/service/input/CheckoutAtomParamInputAdapter.kt
@@ -69,6 +69,7 @@ class CheckoutAtomParamInputAdapter(
         }
     }
 
+    @Suppress("ALL")
     private fun CheckoutAtomParamInput.byRepositoryIdOrName() = GitCodeAtomParamInputAdapter(
         GitCodeAtomParamInput(
             bkWorkspace = bkWorkspace,
